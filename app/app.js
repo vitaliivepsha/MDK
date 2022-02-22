a49368090af8ddc7685a14ceefcb2f866620cd54
Artem <nicktlibra@gmail.com>
2022-02-22 17:23:24 +0200

    delivery

--- app/app.js
+++ app/app.js
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('./assets/templates/layouts/catalog.html');
     require('./assets/templates/layouts/sitemap.html');
     require('./assets/templates/layouts/policy.html');
+    require('./assets/templates/layouts/delivery.html');
 }
 
 // Depends
